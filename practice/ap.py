c603362a56f400e32246bf9c17349812f4c696f0
Freda <101472460+Fredaekaraefod@users.noreply.github.com>
2023-09-06 16:49:13 +0100

    strings

--- practice/ap.py
+++ practice/ap.py
@@ -1,5 +1,5 @@
 # basic calculator to get sum of 2 numbers
 first = input("What is the first number? ")
 second = input("What is the second number? ")
-sum = int(first) + int(second)
-print(sum)
+sum = float(first) + float(second)
+print("Sum is " + str(sum))
---
+++ practice/string.py
@@ -0,0 +1,8 @@
+course = "py for newbies"
+print(course.upper())  # returns in upper case
+print(course.lower())  # returns in lower case
+print(course.find("Y"))  # used to find the index of  the character 'Y'
+print(course.find("y"))  # used to find the index of the character 'y'
+print(course.find("for"))  # used to find the index of the character 'for'
+print(course.replace("for", "4"))  # used to replace the character 'for' with '4'
+print("python" in course)  # an alternative to .find() function
