27623d08838ea7d43d69fd4e98040806400b1719
Freda <101472460+Fredaekaraefod@users.noreply.github.com>
2023-09-06 21:05:27 +0100

    g

---
+++ practice/ari-op.py
@@ -0,0 +1,14 @@
+print(10 - 3)  # subtraction
+print(10 + 3)  # sum
+print(10 * 3)  #
+# division is 2
+print(10 / 3)  # we'll get a float
+print(10 // 3)  # we'llget an integer
+print(10 % 3)
+print(10**3)
+
+# augmented assignment op
+x = 10
+
+x += 3
+print(x)
---
+++ practice/op_pres.py
@@ -0,0 +1,5 @@
+# logical operATOR
+price = 5
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+print(not price > 10)
